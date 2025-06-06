d7d052572f66ab9b71d463b69adef378675a2efb
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-06 14:15:55 +0100

    sdlfkjvn

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
               <h1 className="text-3xl font-bold text-blue-900">ALINO Nzungu </h1>
               <nav className="space-x-4">
                 <Link href="/" className="text-gray-700 hover:text-blue-600">Accueil</Link>
-                <Link href="/about" className="text-gray-700 hover:text-blue-600">À propos</Link>
+                <Link href="/" className="text-gray-700 hover:text-blue-600">À propos</Link>
                 <Link href="/new" className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded">
                   Ajouter un post
                 </Link>
