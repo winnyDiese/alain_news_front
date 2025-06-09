4f4a8c86b209489b4b818095e3dd5903b393febd
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-09 10:32:37 +0100

    typing code...

--- app/blog/layout.js
+++ app/blog/layout.js
@@ -8,23 +8,23 @@ export default function BlogLayout({ children }) {
   return (
     <div className="bg-gray-100 min-h-screen">
 
-          {/* Header / Navigation */}
-          <header className="bg-white shadow fixed top-0 left-0 right-0 z-50 ">
+        {/* Header / Navigation */}
+        <header className="bg-white shadow fixed top-0 left-0 right-0 z-50 ">
             <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-              <h1 className="text-3xl font-bold text-blue-900">
+                <h1 className="text-3xl font-bold text-blue-900">
                 <Link href="/">ALINO Nzungu</Link>
-              </h1>              
-              
-              <nav className="space-x-4">
+                </h1>              
+                
+                <nav className="space-x-4">
                 <Link href="/new" className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded">
-                  Ajouter un post
+                    Ajouter un post
                 </Link>
-              </nav>
+                </nav>
             </div>
-          </header>
+            </header>
 
-          {children}
+            {children}
 
-        </div>
+    </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -29,22 +29,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="bg-gray-100 min-h-screen">
-
-          {/* Header / Navigation */}
-          <header className="bg-white shadow fixed top-0 left-0 right-0 z-50 ">
-            <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-              <h1 className="text-3xl font-bold text-blue-900">
-                <Link href="/">ALINO Nzungu</Link>
-              </h1>              
-              
-              <nav className="space-x-4">
-                <Link href="/new" className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded">
-                  Ajouter un post
-                </Link>
-              </nav>
-            </div>
-          </header>
+        <div className="">
 
           {children}
 
