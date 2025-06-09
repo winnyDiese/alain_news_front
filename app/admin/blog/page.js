f29a70fa956632a4da1d718cd6f16fed53bbdb98
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-09 18:40:59 +0100

    sledkjrvlbkb

--- app/admin/blog/page.js
+++ app/admin/blog/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ScrollText, MessageCircle, PlusCircle } from "lucide-react";
 import Image from "next/image";
 
