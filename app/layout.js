87ee8583dcc200bc9424666e2bbd2ec0b8908639
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-09 09:01:31 +0100

    render title link

--- app/layout.js
+++ app/layout.js
@@ -28,9 +28,11 @@ export default function RootLayout({ children }) {
           {/* Header / Navigation */}
           <header className="bg-white shadow fixed top-0 left-0 right-0 z-50 ">
             <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-              <h1 className="text-3xl font-bold text-blue-900">ALINO Nzungu </h1>
+              <h1 className="text-3xl font-bold text-blue-900">
+                <Link href="/">ALINO Nzungu</Link>
+              </h1>              
+              
               <nav className="space-x-4">
-               
                 <Link href="/new" className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded">
                   Ajouter un post
                 </Link>
