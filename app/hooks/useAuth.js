be11031793ebf8a2e5234abe68146d0f354395ab
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-11 18:47:16 +0100

    adding hooks/useAuth.js page

---
+++ app/hooks/useAuth.js
@@ -0,0 +1,14 @@
+// hooks/useAuth.js
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function useAuth() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+    }
+  }, []);
+}
