fc3c67c03c6d12b954faba2985e36d6e1804cd6a
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-11 18:50:13 +0100

    sdfbkniu

--- app/proteg/page.js
+++ app/proteg/page.js
@@ -1,5 +1,6 @@
 "use client";
-import useAuth from "@/hooks/useAuth";
+// import useAuth from "../hooks/useAuth";
+import useAuth from "../hooks/useAuth";
 
 export default function AdminPage() {
   useAuth();
