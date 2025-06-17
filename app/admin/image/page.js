7a505bc235a731acddda9939f3d9c76c79bf16d1
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-17 10:24:57 +0100

    sdfvlkn

--- app/admin/image/page.js
+++ app/admin/image/page.js
@@ -1,4 +1,6 @@
 "use client";
+
+
 import { CldImage } from 'next-cloudinary';
 
 // By default, the CldImage component applies auto-format and auto-quality to all delivery URLs for optimized delivery.
