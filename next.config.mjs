f313130fe36de0ea0025cc41becea413a59005c3
Pascal ABEDI <abedipascal2@gmail.com>
2025-06-18 08:59:32 +0100

    config nex.config file for show iamge

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com'], },
+    domains: ['images.unsplash.com', 'source.unsplash.com','res.cloudinary.com'], },
 };
 
 export default nextConfig;
